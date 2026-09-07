8a812e5cf9382b24d3bf18edf476b626057c1af9
RobynaN <robyna@robyna.eu>
2026-09-07 18:12:49 +0200

    ruby's
    week 1

--- week 1 - hello world/sketch.js
+++ week 1 - hello world/sketch.js
@@ -40,7 +40,35 @@ function draw() {
    fill(000);
   rect(50, 310, 30, 30);
 
+  fill(255, 255, 255);
+  
+  leftHouse = 50;
+  widthHouse = 200;
+  heightHouse = 100;
+ topHouse = 490;
 
+  rect(leftHouse, topHouse, widthHouse, heightHouse);
+  
+
+  fill(255, 255, 255)
+  x1= leftHouse + widthHouse  / 2;
+  y1= topHouse - widthHouse /2;
+ x3= leftHouse; 
+ y3= topHouse; 
+ x2 = widthHouse + leftHouse;
+ y2= topHouse;
+  triangle(x1, y1, x2, y2, x3, y3)
+
+  
+  fill(253, 253, 253)
+  rect(330, 100, 40, 100);
+
+  fill(242, 25, 26)
+  ellipse(350, 120, 25, 25);
+   fill(242, 240,26)
+  ellipse(350, 150, 25, 25);
+  fill(62, 242, 26)
+  ellipse(350, 180, 25, 25);
 
  
  fill(000);
