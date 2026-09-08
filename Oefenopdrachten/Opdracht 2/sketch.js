7aae978b2ca85229f4f3cdf7b7d9ffd2cc4cd988
RobynaN <robyna@robyna.eu>
2026-09-08 15:25:31 +0200

    oefenopdracht 2
    oefenopdracht 2

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,33 @@
+
+let X = 100;
+let a = 20;
+let b = 10;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(225, 225, 225);
+  text("Hello World", 20,20);
+  
+  let y = 80;
+  /*berekeningen*/
+  /*variable voor alle calculaties, de let y = 80 laat de variable's automatisch + 80 
+  zonder dat je het de heletijd moet typen*/
+  optellen = a + b;
+  text("optellen: " + optellen, 20, y);
+  vermenigvuldigen = a * b;
+ 
+  y = y + 20;
+  text("vermenigvuldigen: " + vermenigvuldigen, 20, y);
+  aftrekken = a - b;
+  
+  y = y + 20;
+  text("aftrekken: " + aftrekken, 20, y);
+  delen = a / b;
+  y = y + 20;
+  text("delen: " + delen, 20, y);
+
 }
+
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,14 @@
+
+let circleX = 100;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  circleX = 0;
 }
 
 function draw() {
-  background(220);
+  background(225, 225, 225);
+  fill(255, 0, 0);
+  ellipse(circleX, 150, 50, 50);
+  circleX = circleX + 2;
 }
