a28d0634a9f473dc02661cbe3f0a20cfe10443dc
RobynaN <robyna@robyna.eu>
2026-09-14 13:46:37 +0200

    week 2 dynamisch landschap
    2.6

--- Week 2 Dynamisch Landschap/sketch.js
+++ Week 2 Dynamisch Landschap/sketch.js
@@ -14,8 +14,8 @@ let light = 0;
 function keyPressed() { 
   if (keyCode == ENTER) {
     light = light + 1;
-    // Nederlandse volgorde: rood, rood + oranje, groen,
-    if (light > 3)
+    // Stoplichtvolgorde: rood, groen, oranje
+    if (light > 2)
       light = 0;
   }
 }
@@ -178,12 +178,11 @@ stroke(1)
    stroke(1);
    fill(60, 60, 60);
    rect(700, 200, 40, 120);
- //verander het nog ff naar een nederlandse stoplicht reminder ! 
-  fill((light == 0 || light == 1) ? color(255, 0, 0) : color(80, 0, 0));
+  fill(light == 0 ? color(255, 0, 0) : color(80, 0, 0));
    circle(720, 220, 25);
-  fill((light == 1 || light == 3) ? color(255, 128, 0) : color(80, 50, 0));
+  fill(light == 1 ? color(0, 255, 0) : color(0, 80, 0));
    circle(720, 260, 25);
-   fill(light == 2 ? color(0, 255, 0) : color(0, 80, 0));
+  fill(light == 2 ? color(255, 128, 0) : color(80, 50, 0));
    circle(720, 300, 25);
 
    fill(60, 60, 60);
@@ -209,9 +208,9 @@ stroke(1)
  const stopLine = 680;
  const carFront = car + 220;
  const approachingStopLine = car < stopLine && carFront >= stopLine - 100;
- if ((light == 0 || light == 1) && car < stopLine && carFront >= stopLine) {
+ if (light == 0 && car < stopLine && carFront >= stopLine) {
   carspeed = 0;
- } else if (light == 3 && approachingStopLine) {
+ } else if (light == 2 && approachingStopLine) {
   // Rem rustig af wanneer het stoplicht oranje is.
   carspeed = max(0, carspeed - 0.1);
  } else {
