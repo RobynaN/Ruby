2e5b70c1b061eab79db843686ec484bc43a997a4
RobynaN <robyna@robyna.eu>
2026-09-21 12:41:11 +0200

    week 3 boter kaas en eieren

--- Week 3 boter kaas en eieren/sketch.js
+++ Week 3 boter kaas en eieren/sketch.js
@@ -9,16 +9,25 @@ function draw() {
   stroke(0);
   strokeWeight(16);
   fill(128);
-  rect(100, 50, 300, 300, 10);
+  rect(100, 50, 300, 300, 20);
   
   stroke(0);
   strokeWeight(8);
-  rect(196, 50, 8, 300, 4);
-  rect(296, 50, 8, 300, 4);
-  rect(100, 146, 300, 8, 4);
-  rect(100, 246, 300, 8, 4);
+  rect(196, 50, 8, 300, 40);
+  rect(296, 50, 8, 300, 40);
+  rect(100, 146, 300, 8, 40);
+  rect(100, 246, 300, 8, 40);
 
   //de speelvakjes. 
+  if (mouseX >= 100 && mouseX < 400 && mouseY >= 50 && mouseY < 350) {
+    let kolom = floor((mouseX - 100) / 100);
+    let rij = floor((mouseY - 50) / 100);
+
+    noStroke();
+    fill(80);
+    rect(105 + kolom * 100, 55 + rij * 100, 90, 90);
+  }
+  
 
 
 }
