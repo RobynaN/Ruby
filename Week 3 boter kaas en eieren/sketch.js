37765739be72ed7563fb159ee8240e5a439779ec
RobynaN <robyna@robyna.eu>
2026-09-22 18:47:22 +0200

    week 3 boter kaas en eieren

--- Week 3 boter kaas en eieren/sketch.js
+++ Week 3 boter kaas en eieren/sketch.js
@@ -1,3 +1,13 @@
+// let bord;
+// let speler = 1;
+// let afgelopen = false;
+// let winnaar = 0;
+
+// let winRij = -1;
+// let winKolom = -1;
+// let winDiagonaal = 0;
+
+
 function setup() {
   createCanvas(500, 400);
 }
@@ -24,7 +34,7 @@ function draw() {
   rect(205, 260, 90, 90,10);
   rect(310, 260, 90, 90,10);
 
-  //de speelvakjes. 
+  //de mouse
   if (mouseX >= 100 && mouseX < 400 && mouseY >= 50 && mouseY < 350) {
     let kolom = floor((mouseX - 100) / 100);
     let rij = floor((mouseY - 50) / 100);
@@ -33,7 +43,34 @@ function draw() {
     fill(80);
     rect(100 + kolom * 100 + 5, 50 + rij * 100 + 5, 90, 90,10);
   }
-//de x en o's
+
+//  //winnende streep
+//  if(afgelopen && winnaar != 0) {
+//   stroke('black');
+//   strokeWeight(8);
+
+//   //horizontale lijn
+
+//   if(winRij != -1) {
+//     line(110, 95, winRij * 105, 390, 95 + winRij * 105);
+//   }
+
+//   //verticale lijn
+//   if(winKolom != -1) {
+//     line(145 + winKolom * 105, 60, 145 + winKolom * 105, 340);
+//   }
+
+//   //lijn??
+//   if(winDiagonaal == 1) {
+//     line(110, 60, 390, 340);
+//   }
+
+//   //lijn 2 
+//   if(winDiagonaal == 2) {
+//     line(390, 60, 110, 340);
+//  }
+//  }
+  //tekst 
 
 
 }
