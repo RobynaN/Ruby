8fcebbf97cd390876cf0fae3367c52dfa8ad1b9a
RobynaN <robyna@robyna.eu>
2026-09-22 20:25:03 +0200

    week 3 boter kaas en eieren

--- Week 3 boter kaas en eieren/sketch.js
+++ Week 3 boter kaas en eieren/sketch.js
@@ -1,16 +1,13 @@
-let bord;
+let bord = 0;
 let speler = 1;
 let afgelopen = false;
 let winnaar = 0;
 
-// let winRij = -1;
-// let winKolom = -1;
-// let winDiagonaal = 0;
 
 
 function setup() {
-  createCanvas(500, 400);
-  bord = Array.from({lenght: 3 }, () => Array(3).fill(0));
+  createCanvas(550, 500);
+  bord = Array.from({length: 3 }, () => Array(3).fill(0));
 }
 
 function draw() {
@@ -45,43 +42,16 @@ function draw() {
     rect(100 + kolom * 100 + 5, 50 + rij * 100 + 5, 90, 90,10);
   }
   //vakjes kleuren
-
-  for(let rij = 0: rij < 3; rij++) {
+  for(let rij = 0; rij < 3; rij++) {
     for(let kolom = 0; kolom < 3; kolom++) {
-      if(bord[rij][kolom]!== 0) {
-        fill(bord[rij][kolom] === 1 ? 'blauw' : 'rood');
+      if (bord[rij][kolom] !== 0) {
+        fill(bord[rij][kolom] === 1 ? 'blue' : 'red');
         noStroke();
         rect(100+ kolom * 100 + 5, 50 + rij * 100 + 5, 90, 90, 10);
       }
     }
   }
 
-//  //winnende streep
-//  if(afgelopen && winnaar != 0) {
-//   stroke('black');
-//   strokeWeight(8);
-
-//   //horizontale lijn
-
-//   if(winRij != -1) {
-//     line(110, 95, winRij * 105, 390, 95 + winRij * 105);
-//   }
-
-//   //verticale lijn
-//   if(winKolom != -1) {
-//     line(145 + winKolom * 105, 60, 145 + winKolom * 105, 340);
-//   }
-
-//   //lijn??
-//   if(winDiagonaal == 1) {
-//     line(110, 60, 390, 340);
-//   }
-
-//   //lijn 2 
-//   if(winDiagonaal == 2) {
-//     line(390, 60, 110, 340);
-//  }
-//  }
   //tekst 
 fill(0);
 textAlign(CENTER, CENTER);
@@ -90,12 +60,12 @@ if(afgelopen) {
   text(winnaar === 0 ? 'Gelijkspel! klik om opnieuw te beginnen' : 'speler' + winnaar + 'winnaar! klik om opnieuw te beginnen', 250, 250);
 
 } else{
-  text('speler' + speler + 'aan de beurt'(' + ( speler === 1 ? 'blauw' : 'rood) +')', 250, 380);
+  text('speler' + speler + 'aan de beurt' + ( speler === 1 ? 'blauw' : 'rood') +')', 250, 380);
    }
 }
 function mousePressed() {
   if(afgelopen) {
-    bord = Array.from({lenght : 3}, () => Array(3).fill(0));
+    bord = Array.from({length : 3}, () => Array(3).fill(0));
     speler = 1;
     winnaar = 0;
     afgelopen = false; 
@@ -108,7 +78,7 @@ function mousePressed() {
 
   let kolom = floor((mouseX - 100) / 100);
   let rij = floor((mouseY - 50) / 100); 
-  if(bord[rij][kolom] !== 0) return;
+  if (bord[rij][kolom] !== 0) return;
 
   bord[rij][kolom] = speler; 
 
@@ -124,7 +94,7 @@ function mousePressed() {
 }
 
 function heeftGewonnen(speler) {
-  for(let i = o ; i < 3; i++) { 
+  for(let i = 0; i < 3; i++) { 
     if(bord[i][0] === speler && bord[i][1] === speler && bord[i][2] === speler) return true; 
     if(bord[0][i] === speler && bord[1][i] === speler && bord[2][i]=== speler) return true;
   
