f891bc2220c156bb3feb55fa9eb2189844ddf289
RobynaN <robyna@robyna.eu>
2026-09-25 10:09:19 +0200

    les opdr niet belangrijk was iets aan t proberen

--- les opdracht/sketch.js
+++ les opdracht/sketch.js
@@ -9,33 +9,6 @@ function draw() {
   drawStar(drawingContext, 150, 150, 5, 70, 30);
 }
 
-function drawStar(ctx, cx, cy, spikes, outerRadius, innerRadius) {
-  let rotation = -Math.PI / 2;
-  const step = Math.PI / spikes;
-
-  ctx.beginPath();
-  ctx.moveTo(
-    cx + Math.cos(rotation) * outerRadius,
-    cy + Math.sin(rotation) * outerRadius
-  );
-
-  for (let i = 0; i < spikes; i++) {
-    rotation += step;
-    ctx.lineTo(
-      cx + Math.cos(rotation) * innerRadius,
-      cy + Math.sin(rotation) * innerRadius
-    );
-    rotation += step;
-    ctx.lineTo(
-      cx + Math.cos(rotation) * outerRadius,
-      cy + Math.sin(rotation) * outerRadius
-    );
-  }
-
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
-}
 
 
 
